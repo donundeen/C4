22230c868a7c055eca46aae1548844a803c6ce36
don undeen <don.undeen@metmuseum.org>
2015-11-11 16:29:41 -0500

    rearranging the files that need to override jibs files.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,5 @@
 sudo nginx -c /Users/donundeen/htdocs/C4/server_configs/nginx/nginx.conf
 PORT=3003 JSBIN_CONFIG=/Users/donundeen/htdocs/C4/server_configs/jsbin/config.local.json JSBIN_PROXY=on jsbin &
 JSBIN_CONFIG=/Users/donundeen/htdocs/C4/server_configs/jsbin/config.api.json  jsbin &
+cd ../meteor/C4
+meteor
