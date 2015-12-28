a831f938f6f286b24134161d8c9df7382b6e2c42
don undeen <don.undeen@metmuseum.org>
2015-12-28 15:58:27 -0700

    refactored common functions into their own file.

--- meteor/C4/C4.js
+++ meteor/C4/C4.js
@@ -1,68 +1,7 @@
 
 Widgets = new Mongo.Collection("widgets");
 
-pageinfo = setWidgetDefaults = null;
-
 if (Meteor.isClient) {
-
-
-  pageinfo = function(){
-    var pagetype = "";
-    var pageid = "";
-    var pathname = window.location.pathname;
-    var split = pathname.split("/");
-    split.shift();
-    var pageurl =  split.join("/");    
-
-    if(split.length > 0){
-      pagetype = split.shift();
-    }
-    if(split.length > 0){
-      pageid = split.shift();
-    }
-    pageid = pageid.replace(/:script/, "");
-    return {pageurl : pageurl,
-            pagetype : pagetype,
-            pageid : pageid};
-
-  }
-
-  setWidgetDefaults = function(doc){
-
-    console.log("mapping");
-    if(typeof doc.displayWidth === "undefined" || !doc.displayWidth || doc.displayWidth.trim() == "" || doc.displayWidth == "width" || doc.displayWidth == "default"){
-      doc.displayWidth = "default";
-      doc.displayUsableWidth = "";
-    }else{
-      doc.displayUsableWidth = doc.displayWidth;
-    }
-    if(typeof doc.displayHeight === "undefined" || !doc.displayHeight || doc.displayHeight.trim() == "" || doc.displayHeight == "height"  || doc.displayHeight == "default"){
-      doc.displayHeight = "default";
-      doc.displayUsableHeight = "";
-    }else{
-      doc.displayUsableHeight = doc.displayHeight;
-    }
-    if(typeof doc.widgetStyle === "undefined" || !doc.widgetStyle || doc.widgetStyle.trim() == "" || doc.widgetStyle == "css" || doc.widgetStyle == "default"){
-      doc.widgetStyle = "default";
-      doc.usableWidgetStyle = "";
-    }else{
-      doc.usableWidgetStyle = doc.widgetStyle;
-    }
-    if(!doc.createdBy){
-      doc.createdBy = {};
-    }
-
-    if(doc.displayUsableHeight.match(/px/)){
-      var height = doc.displayUsableHeight.replace(/px/,"");
-      doc.jsbinHeight = height - 20;
-      doc.jsbinHeight += "px";
-    }else{
-      doc.jsbinHeight = "";
-    }
-
-    return doc;
-  }
-
 
   Meteor.startup(function(){
    $(window).bind('beforeunload', function() {
