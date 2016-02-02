18bf0063d8adab819a807a60f55bb35c04bb4769
don undeen <donundeen@gmailcom>
2016-02-02 15:51:30 -0500

    dumping and restoring jsbin database

--- scripts/meteor_db_restore.sh
+++ scripts/meteor_db_restore.sh
@@ -1,1 +1,3 @@
 /usr/local/bin/mongorestore -hlocalhost:3001 -d meteor  ../dbbackups/meteor
+cp /usr/local/lib/node_modules/jsbin/jsbin.sqlite ../dbbackups/jsbin.sqlite.orig
+cp ../dbbackups/jsbin.sqlite  /usr/local/lib/node_modules/jsbin/jsbin.sqlite
