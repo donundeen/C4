67c44d7d45673b60f315215b4891af4e7b9ae36c
don undeen <donundeen@c5.boomhifive.com>
2016-02-07 20:05:38 -0600

    using forever to start and stop node scripts

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,5 +1,11 @@
-sudo nginx -s stop
+C5_HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+source $C5_HOME_DIR/scripts/env_config.sh
+sudo nginx -s quit
 kill -9 $(ps aux | grep 'node /usr/local/bin/jsbin' | awk '{print $2}')
-kill -9 $(ps aux | grep 'jsbin_proxy' | awk '{print $2}')
-kill -9 $(ps aux | grep 'web_proxy' | awk '{print $2}')
-kill -9 $(ps aux | grep 'giphy_proxy' | awk '{print $2}')
+cd ../web_proxy
+forever stop web_proxy.server.js &
+cd ../jsbin_proxy
+forever stop jsbin_proxy.server.js &
+cd ../giphy_proxy
+forever stop giphy_proxy.server.js &
+
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,15 +1,16 @@
 C5_HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 source $C5_HOME_DIR/scripts/env_config.sh
-sudo nginx -c $C5_HOME_DIR/server_configs/nginx/nginx.conf
+echo sudo nginx -c $C5_NGINX_CONFIG 
+sudo nginx -c $C5_NGINX_CONFIG
 echo PORT=3003 JSBIN_CONFIG=$C5_JSBIN_EMBED_CONFIG JSBIN_PROXY=on jsbin &
 PORT=3003 JSBIN_CONFIG=$C5_JSBIN_EMBED_CONFIG JSBIN_PROXY=on jsbin &
 echo JSBIN_CONFIG=$C5_JSBIN_API_CONFIG jsbin &
 JSBIN_CONFIG=$C5_JSBIN_API_CONFIG jsbin &
 cd ../web_proxy
-node web_proxy.server.js &
+forever start web_proxy.server.js &
 cd ../jsbin_proxy
-node jsbin_proxy.server.js &
+forever start jsbin_proxy.server.js &
 cd ../giphy_proxy
-node giphy_proxy.server.js &
+forever start giphy_proxy.server.js &
 cd ../meteor/C4
 meteor
