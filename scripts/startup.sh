f338722dfb36528c4b84dace3977b4b62433372c
don undeen <don.undeen@metmuseum.org>
2016-02-07 20:12:30 -0500

    local config files, and a revised startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,9 +1,10 @@
 C5_HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+source $C5_HOME_DIR/scripts/env_config.sh
 sudo nginx -c $C5_HOME_DIR/server_configs/nginx/nginx.conf
-echo PORT=3003 JSBIN_CONFIG=$C5_HOME_DIR/server_configs/jsbin/config.embed.json JSBIN_PROXY=on jsbin &
-PORT=3003 JSBIN_CONFIG=$C5_HOME_DIR/server_configs/jsbin/config.embed.json JSBIN_PROXY=on jsbin &
-echo JSBIN_CONFIG=$C5_HOME_DIR/server_configs/jsbin/config.api.json  jsbin &
-JSBIN_CONFIG=$C5_HOME_DIR/server_configs/jsbin/config.api.json  jsbin &
+echo PORT=3003 JSBIN_CONFIG=$C5_JSBIN_EMBED_CONFIG JSBIN_PROXY=on jsbin &
+PORT=3003 JSBIN_CONFIG=$C5_JSBIN_EMBED_CONFIG JSBIN_PROXY=on jsbin &
+echo JSBIN_CONFIG=$C5_JSBIN_API_CONFIG jsbin &
+JSBIN_CONFIG=$C5_JSBIN_API_CONFIG jsbin &
 cd ../web_proxy
 node web_proxy.server.js &
 cd ../jsbin_proxy
